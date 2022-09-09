903714e3b17d5ad1907482ca70816aeb9e876e9d
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-09-09 18:44:23 -0400

    Reduced to Atoms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Job Application Tracker</p>
-      </header>
+      <h1>Job Application Tracker</h1>
     </div>
   );
 }
