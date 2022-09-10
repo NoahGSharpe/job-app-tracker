e70d5109c17f04c20c571d9f3347e52713561b3a
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-09-10 12:40:43 -0400

    Sent state vars down to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import JobAppList from "./components/JobAppList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [jobApps, setJobApps] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Job Application Tracker</h1>
       </header>
-      <Form />
-      <JobAppList/>
+      <Form inputText={inputText} setInputText={setInputText} jobApps={jobApps} setJobApps={setJobApps} />
+      <JobAppList jobApps={jobApps} />
     </div>
   );
 }
