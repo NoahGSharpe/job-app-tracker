993da4b898d67478bec826b4a73b0034f01ac764
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-09-10 16:08:46 -0400

    Added Local Storage
    Data now persists on refresh

--- src/App.js
+++ src/App.js
@@ -3,38 +3,29 @@ import "./App.css";
 import Form from "./components/Form";
 import JobAppList from "./components/JobAppList";
 
+const LOCAL_STORAGE_KEY = "JobAppsLocalStorage";
+
 function App() {
   const [inputText, setInputText] = useState("");
-  const [jobApps, setJobApps] = useState([]);
+  const [jobApps, setJobApps] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+  );
 
   useEffect(() => {
-    getLocal();
-  }, [])
-
-  useEffect(() => {
-    saveLocal();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [jobApps])
-
-  const saveLocal = () => {
-    localStorage.setItem("JobApps", JSON.stringify(jobApps));
-  }
-  const getLocal = () => {
-    if (localStorage.getItem("JobApps") === null){
-      localStorage.setItem("JobApps", JSON.stringify([]));
-    } else {
-      let localJobApps = JSON.parse(localStorage.getItem("JobApps"));
-      setJobApps(localJobApps);
-    }
-  }
-
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(jobApps));
+  }, [jobApps]);
 
   return (
     <div className="App">
       <header>
         <h1>Job Application Tracker</h1>
       </header>
-      <Form inputText={inputText} setInputText={setInputText} jobApps={jobApps} setJobApps={setJobApps} />
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        jobApps={jobApps}
+        setJobApps={setJobApps}
+      />
       <JobAppList jobApps={jobApps} setJobApps={setJobApps} />
     </div>
   );
