1bf30c41946663f2007e8e6188c1ef7cd397444c
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-09-10 12:40:21 -0400

    Setup Form to change state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-
-function Form(){
-    return(
-        <form>
-            <input type="text" />
-            <button type="submit">Submit</button>
-        </form>
-    )
+function Form({ inputText, setInputText, jobApps, setJobApps }) {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitJobAppHandler = (e) => {
+    e.preventDefault();
+    setJobApps([
+      ...jobApps,
+      { companyName: inputText, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+  return (
+    <form>
+      <input value={inputText} onChange={inputTextHandler} type="text" />
+      <button onClick={submitJobAppHandler} type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
-
 
 export default Form;
